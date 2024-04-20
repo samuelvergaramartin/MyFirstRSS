8ea526accb5bf2ac7481ee7cafb7a8b288201363
Sam170703 <sam170703dev@gmail.com>
2024-04-20 13:38:00 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.set('views', '.');
 app.set('view engine', 'php');
 app.all(/.+\.php$/, phpExpress.router);
 
-app.use("/", (req, res) => res.render("index"));
+app.use("/", (req, res) => res.render("public/index"));
 
 app.listen(port, () => {
   console.log(`Servidor listo y escuchando en el puerto ` + port);
