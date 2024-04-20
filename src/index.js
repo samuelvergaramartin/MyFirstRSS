b17872297229f018a1e887c10d2735758c80d729
Sam170703 <sam170703dev@gmail.com>
2024-04-20 13:36:06 +0200

    Updated index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const phpExpress = require('php-express');
+const port = 8080;
+const app = express();
+
+app.engine('php', phpExpress.engine);
+app.set('views', '.');
+app.set('view engine', 'php');
+app.all(/.+\.php$/, phpExpress.router);
+
+app.use("/", (req, res) => res.render("index"));
+
+app.listen(port, () => {
+  console.log(`Servidor listo y escuchando en el puerto ` + port);
+});
