9a043dea881a384f1893f99f9a066f10fd1ef1a0
Sam170703 <sam170703dev@gmail.com>
2024-04-21 14:51:35 +0200

    Updated exercisepart1.php

--- src/public/pages/exercisepart1.php
+++ src/public/pages/exercisepart1.php
@@ -1,45 +1,8 @@
-<?php 
-const normalMode = "Normal";
-const maintenanceMode = "Maintenance";
-const mainCSSPath = "../styles/main.css";
-const normalCSSPath = "./styles/exercisepart1.css";
-const maintenanceCSSPath = "../styles/maintenance.css";
-const author = "Samuel Vergara Martín";
-
-const normalCode = "
-    <h1>Ejercicio parte 1<h1>
-    <div id='blog-feed'></div>
-    <script src='test.js'></script>
-";
-const maintenanceCode = "
-    <h1>Sección en mantenimiento. Disculpe las molestias.</h1>
-";
-
-$mode = maintenanceMode;
+<?php
+    const normalCode = "
+        <h1>Ejercicio parte 1<h1>
+        <div id='blog-feed'></div>
+        <script src='test.js'></script>
+    ";
 ?>
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejercicio RSS Parte 1</title>
-    <link rel="stylesheet" href=<?php
-        if($mode == normalMode) {
-            echo normalCSSPath;
-        }
-        if($mode == maintenanceMode) {
-            echo maintenanceCSSPath;
-        }
-    ?>>
-</head>
-<body>
-    <?php 
-        if($mode == normalMode) {
-            echo normalCode;
-        }
-        if($mode == maintenanceMode) {
-            echo maintenanceCode;
-        }
-    ?>
-</body>
-</html>
+<h1 id="titulo">Parte 1 RSS</h1>
