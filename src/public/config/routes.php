0912be1fb83cc9bbab8a5e79d501f1b8f7a1ced1
Sam170703 <sam170703dev@gmail.com>
2024-04-21 22:14:39 +0200

    Updated routes.php

--- src/public/config/routes.php
+++ src/public/config/routes.php
@@ -1,6 +1,10 @@
 <?php
+    $lectura = file_get_contents("../server.conf");
+    $posicion = strpos($lectura, 'PORT=');
+    $port = substr($lectura, $posicion + 5);
+
     const mainPage = "mainPage";
     const part1Page = "part1Page";
     const part2Page = "part2Page";
-    const headerURL = "http://rss.samuelpc.local:8080";
+    $headerURL = "http://rss.samuelpc.local:" . $port;
 ?>
