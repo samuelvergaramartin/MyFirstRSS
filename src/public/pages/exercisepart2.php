6c5073f2a8d8241c3ab2e0bf70b494585bc96358
Sam170703 <sam170703dev@gmail.com>
2024-04-21 20:41:33 +0200

    Updated exercisepart2.php

--- src/public/pages/exercisepart2.php
+++ src/public/pages/exercisepart2.php
@@ -1,3 +1,3 @@
 <h1 id="titulo">Ejercicio de RSS Parte 2</h1>
 <div id='blog-feed'></div>
-<script src='http://rss.samuelpc.local:8080/loadRSS'></script>
+<script src=<?php echo headerURL;?>/loadRSS></script>
