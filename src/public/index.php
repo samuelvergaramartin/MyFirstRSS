5bca43fc754c8a8343fa491c60b6f74933d58c73
Sam170703 <sam170703dev@gmail.com>
2024-04-21 12:28:17 +0200

    Updated index.php

--- src/public/index.php
+++ src/public/index.php
@@ -2,21 +2,14 @@
     require("./config/modes.php");
     require("./config/routes.php");
     require("./config/codes.php");
+    require("./config/styles.php");
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href=<?php echo mainCSSPath ?>>
-    <link rel="stylesheet" href=<?php
-        if($mode == normalMode) {
-            echo normalCSSPath;
-        }
-        if($mode == maintenanceMode) {
-            echo maintenanceCSSPath;
-        }
-    ?>>
+    <?php echo mainCSSStyle ?>
     <title>Ejercicio RSS</title>
 </head>
 <body>
