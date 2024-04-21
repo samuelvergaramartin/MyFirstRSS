6331bf2e6dff651fa926b051a077779d6b0f6dfa
Sam170703 <sam170703dev@gmail.com>
2024-04-21 15:10:19 +0200

    Updated exercisepart1.php

--- src/public/pages/exercisepart1.php
+++ src/public/pages/exercisepart1.php
@@ -1,9 +1,2 @@
-<?php
-    const normalCode = "
-        <h1>Ejercicio parte 1<h1>
-        <div id='blog-feed'></div>
-        <script src='test.js'></script>
-    ";
-?>
 <h1 id="titulo">Ejercicio RSS Parte 1</h1>
 <a href="http://rss.samuelpc.local:8080/main.xml">Link al RSS</a>
