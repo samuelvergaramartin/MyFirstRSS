2125c1352e5f6c1c49c59ee98127a9b1304f7e2d
Sam170703 <sam170703dev@gmail.com>
2024-04-21 12:30:39 +0200

    Updated styles.php

--- src/public/config/styles.php
+++ src/public/config/styles.php
@@ -10,4 +10,21 @@
             }
         </style>
     ";
+    const normalCSSStyle = "
+        #titulo {
+            color: cyan;
+            text-align: center;
+        }
+        
+        a {
+            font-size: 40px;
+            color: red;
+        }
+    ";
+    const maintenanceCSSStyle = "
+        h1 {
+            color: red;
+            text-align: center;
+        }
+    ";
 ?>
