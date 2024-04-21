3b3ae5fc1810bd3bd8ef17a3ab2e96a67c4b1911
Sam170703 <sam170703dev@gmail.com>
2024-04-21 20:51:13 +0200

    Updated codes.php

--- src/public/config/codes.php
+++ src/public/config/codes.php
@@ -1,9 +1,9 @@
 <?php
     const mainPageCode = "
-    <h1 style='color: cyan; text-align: center; font-size: 40px;'>Mis ejercicios de RSS. Samuel Vergara Martín</h1>
+        <h1 style='color: cyan; text-align: center; font-size: 40px;'>Mis ejercicios de RSS. Samuel Vergara Martín</h1>
 
-    <a style='font-size: 40px; color: red; 'href='" . headerURL ."/src/public/index.php?source=exRSSPart1'>Parte 1 del ejercicio (con el rss en el xml)</a> <br>
-    <a style='font-size: 40px; color: red; ' href='" . headerURL . "/src/public/index.php?source=exRSSPart2'>Parte 2 del ejercicio (enlaces de los rss de 3 sitios diferentes)</a>
+        <a style='font-size: 40px; color: red; 'href='" . headerURL ."/src/public/index.php?source=exRSSPart1'>Parte 1 del ejercicio (con el rss en el xml)</a> <br>
+        <a style='font-size: 40px; color: red; ' href='" . headerURL . "/src/public/index.php?source=exRSSPart2'>Parte 2 del ejercicio (enlaces de los rss de 3 sitios diferentes)</a>
     ";
     const maintenanceCode = "
         <h1>Página web en mantenimiento. Disculpe las molestias.</h1>
