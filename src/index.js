c8fce2b0bbf9e541d341b86dbaad51e9af1d0f62
Sam170703 <sam170703dev@gmail.com>
2024-04-21 21:50:14 +0200

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,28 @@ var port;
 const app = express();
 const { spawn } = require('child_process');
 
-spawn("./portReader.sh").stdout.on("data", (data) => port = data);
+spawn(__dirname + "/portReader.sh").stdout.on("data", (data) => port = Number(`${data}`));
+setTimeout(() => {
+    app.engine('php', phpExpress.engine);
+    app.set('views', '.');
+    app.set('view engine', 'php');
+    app.all(/.+\.php$/, phpExpress.router);
 
-app.engine('php', phpExpress.engine);
-app.set('views', '.');
-app.set('view engine', 'php');
-app.all(/.+\.php$/, phpExpress.router);
-
-app.get("/main.xml", (req, res) => {
-    res.sendFile(__dirname + "/main.xml");
-});
-app.get("/loadRSS", (req, res) => {
-    res.sendFile(__dirname + "/renderer.js");
-});
-app.use("/", (req, res) => {
-    const parametros = req.query;
-    res.render("src/public/index", {
-        parametros: parametros
+    app.get("/main.xml", (req, res) => {
+        res.sendFile(__dirname + "/main.xml");
     });
-});
+    app.get("/loadRSS", (req, res) => {
+        res.sendFile(__dirname + "/renderer.js");
+    });
+    app.use("/", (req, res) => {
+        const parametros = req.query;
+        res.render("src/public/index", {
+            parametros: parametros
+        });
+    });
 
 
-app.listen(port, () => {
-  console.log(`Servidor listo y escuchando en el puerto ` + port);
-});
+    app.listen(port, () => {
+        console.log(`Servidor listo y escuchando en el puerto ` + port);
+    });
+}, 1000);
