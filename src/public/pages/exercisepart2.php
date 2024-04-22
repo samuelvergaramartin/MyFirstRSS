a3233e49a9b817fab0ae62f09a33deb43739e8cc
Sam170703 <sam170703dev@gmail.com>
2024-04-22 09:51:13 +0200

    Updated exercisepart2.php

--- src/public/pages/exercisepart2.php
+++ src/public/pages/exercisepart2.php
@@ -1,3 +1,4 @@
 <h1 id="titulo">Ejercicio de RSS Parte 2</h1>
-<div id='blog-feed'></div>
-<script src=<?php echo $headerURL;?>/loadRSS></script>
+<a href=<?php echo $headerURL;?>/rss1>Link al RSS 1</a>
+<a href=<?php echo $headerURL;?>/rss2>Link al RSS 2</a>
+<a href=<?php echo $headerURL;?>/rss3>Link al RSS 3</a>
