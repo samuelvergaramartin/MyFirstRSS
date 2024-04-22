121e2202b7a40bc15ebfb8baf61c906e18ca94c5
Sam170703 <sam170703dev@gmail.com>
2024-04-22 22:08:22 +0200

    Updated routes.php

--- src/public/config/routes.php
+++ src/public/config/routes.php
@@ -1,10 +1,22 @@
 <?php
+    require("./hosts.php");
     $lectura = file_get_contents("../server.conf");
     $posicion = strpos($lectura, 'PORT=');
     $port = substr($lectura, $posicion + 5);
 
+    $headerURL;
     const mainPage = "mainPage";
     const part1Page = "part1Page";
     const part2Page = "part2Page";
-    $headerURL = "http://rss.samuelpc.local:" . $port;
+
+    switch(host) {
+        case localhost: $headerURL = "http://rss.samuelpc.local" . $port;
+        break;
+        case vpsWithoutCloudflareProxy: $headerURL = "https://exerciserss.netcathost.es:" . $port;
+        break;
+        case vpsWithCloudflareProxy: $headerURL = "https://exerciserss.netcathost.es:";
+        break;
+        default: throw new Error("ERROR: Configuración de host inválida!");
+    }
+    
 ?>
