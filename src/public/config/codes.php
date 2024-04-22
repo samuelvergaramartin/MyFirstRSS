36dc744cb8f3df1a73fcc8a64d260d62ec92e5b3
Sam170703 <sam170703dev@gmail.com>
2024-04-22 13:50:24 +0200

    Updated codes.php

--- src/public/config/codes.php
+++ src/public/config/codes.php
@@ -2,8 +2,10 @@
     $mainPageCode = "
         <h1 id='titulo'>Mis ejercicios de RSS. Samuel Vergara Martín</h1>
 
-        <a href='" . $headerURL ."/src/public/index.php?source=exRSSPart1'>Parte 1 del ejercicio (con el rss en el xml)</a> <br>
-        <a href='" . $headerURL . "/src/public/index.php?source=exRSSPart2'>Parte 2 del ejercicio (enlaces de los rss de 3 sitios diferentes)</a>
+        <ul>
+            <li class='listados'><a href='" . $headerURL ."/src/public/index.php?source=exRSSPart1'>Link parte 1 del ejercicio (con el rss en el xml)</a> <br></li>
+            <li class='listados'><a href='" . $headerURL . "/src/public/index.php?source=exRSSPart2'>Link parte 2 del ejercicio (enlaces de los rss de 3 sitios diferentes)</a></li>
+        </ul>
     ";
     const maintenanceCode = "
         <h1>Página web en mantenimiento. Disculpe las molestias.</h1>
