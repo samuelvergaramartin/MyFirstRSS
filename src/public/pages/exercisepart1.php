c1f28bf9e3b490f7d43932ed8fe4d300f51a3f07
Sam170703 <sam170703dev@gmail.com>
2024-04-22 11:04:49 +0200

    Updated exercisepart1.php

--- src/public/pages/exercisepart1.php
+++ src/public/pages/exercisepart1.php
@@ -1,4 +1,5 @@
 <h1 id="titulo">Ejercicio RSS Parte 1</h1>
+<a href=<?php echo $headerURL; ?>/src/public/index.php?source=main>Volver a inicio</a> <br>
 <a href=<?php 
     echo $headerURL;
 ?>/main.xml>Link al RSS</a>
