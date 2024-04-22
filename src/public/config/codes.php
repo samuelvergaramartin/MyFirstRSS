45dff643a69158e39b64eabd7ef9c4a74872ed5e
Sam170703 <sam170703dev@gmail.com>
2024-04-22 10:45:59 +0200

    Updated codes.php

--- src/public/config/codes.php
+++ src/public/config/codes.php
@@ -11,6 +11,6 @@
     $rssPageCode = "
         <h1 id='titulo'>Ejercicio de RSS Parte 2</h1>
         <div id='blog-feed'></div>
-        <script src=$headerURL/loadRSS></script>
+        <script src='" . $headerURL . "/loadRSS'></script>
     ";
 ?>
