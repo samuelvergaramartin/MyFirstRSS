61bdc601fa1d247ba35fe907d971a26e71717952
Sam170703 <sam170703dev@gmail.com>
2024-04-22 09:23:35 +0200

    Updated codes.php

--- src/public/config/codes.php
+++ src/public/config/codes.php
@@ -1,6 +1,6 @@
 <?php
     $mainPageCode = "
-        <h1>Mis ejercicios de RSS. Samuel Vergara Martín</h1>
+        <h1 id='titulo'>Mis ejercicios de RSS. Samuel Vergara Martín</h1>
 
         <a href='" . $headerURL ."/src/public/index.php?source=exRSSPart1'>Parte 1 del ejercicio (con el rss en el xml)</a> <br>
         <a href='" . $headerURL . "/src/public/index.php?source=exRSSPart2'>Parte 2 del ejercicio (enlaces de los rss de 3 sitios diferentes)</a>
