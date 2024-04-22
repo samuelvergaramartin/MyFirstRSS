664e7c39289b5b7771ba1e04494767ca4421e023
Sam170703 <sam170703dev@gmail.com>
2024-04-22 11:04:23 +0200

    Updated exercisepart2.php

--- src/public/pages/exercisepart2.php
+++ src/public/pages/exercisepart2.php
@@ -1,4 +1,5 @@
 <h1 id="titulo">Ejercicio de RSS Parte 2</h1>
+<a href=<?php echo $headerURL; ?>/src/public/index.php?source=main>Volver a inicio</a> <br>
 <a href=<?php echo $headerURL;?>/rss1>Link al RSS 1</a>
 <a href=<?php echo $headerURL;?>/rss2>Link al RSS 2</a>
 <a href=<?php echo $headerURL;?>/rss3>Link al RSS 3</a>
