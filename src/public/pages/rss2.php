3fa010c4f8545a3c71e0ea1fad9a644d279d614e
Sam170703 <sam170703dev@gmail.com>
2024-04-22 11:00:12 +0200

    Updated rss2.php

--- src/public/pages/rss2.php
+++ src/public/pages/rss2.php
@@ -1,3 +1,33 @@
-<h1 id="titulo">Ejercicio de RSS Parte 2</h1>
-<div id='blog-feed'></div>
-<script src=<?php echo $headerURL;?>/loadRSS></script>
+<?php 
+    require("../config/styles.php");
+    require("../config/modes.php");
+    require("../config/codes.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo mainCSSStyle; ?>
+    <?php 
+        if($mode == maintenanceMode) {
+            echo maintenanceCSSStyle;
+        }
+        if($mode == normalMode) {
+            echo normalCSSStyle;
+        }
+    ?>
+    <title>RSS 2</title>
+</head>
+<body>
+    <script>const rss = 2;</script>
+    <?php 
+        if($mode == normalMode) {
+            echo $rssPageCode;
+        }
+        if($mode == maintenanceMode) {
+            echo maintenanceCode;
+        }
+    ?>
+</body>
+</html>
