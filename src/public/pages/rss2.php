e217bee9bcee3b5fffc1294d7802facc5dd24d85
Sam170703 <sam170703dev@gmail.com>
2024-04-22 11:06:34 +0200

    Updated rss2.php

--- src/public/pages/rss2.php
+++ src/public/pages/rss2.php
@@ -2,6 +2,7 @@
     require("../config/styles.php");
     require("../config/modes.php");
     require("../config/codes.php");
+    require("../config/modes.php");
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,6 +21,7 @@
     <title>RSS 2</title>
 </head>
 <body>
+    <a href=<?php echo $headerURL; ?>/src/public/index.php?source=main>Volver a inicio</a> <br>
     <script>const rss = 2;</script>
     <?php 
         if($mode == normalMode) {
